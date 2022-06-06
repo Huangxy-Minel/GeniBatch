21688bccbdc3ab42f4ac698bbf4d10c1df2dff1f
Huangxy-Minel <huangxy_minel@foxmail.com>
2022-06-06 16:35:06 +0800

    update encryption

---
+++ encryption/encrypt.py
@@ -0,0 +1,18 @@
+import numpy as np
+from federatedml.FATE_Engine.python.bigintengine.gpu.gpu_store import FPN_store
+class BatchEncryption(object):
+    @staticmethod
+    def batchEncrypt(data:np.array, pub_key, batch_schemes):
+        res = []
+        ''''Encrypt a matrix based on a given batch scheme'''
+        col_num = data.shape[1]
+        for row_vec, batch_scheme in zip (data, batch_schemes):
+            # make up zeros
+            max_element_num, split_num = batch_scheme
+            row_vec = np.hstack(row_vec, np.zeros(max_element_num * split_num - col_num))
+            # package into batch number
+            fpn_store = FPN_store.init_from_arr(row_vec, pub_key.n, pub_key.max_int)
+            pen_store = fpn_store.encrypt(pub_key)
+            pen_store = pen_store.obfuscation()
+            res.append(pen_store.get_PEN_ndarray())
+        return res
--- planner/test/test.py
+++ planner/test/test.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 A = [1, 2, 3]
-print(A[: -1])
+A = np.array(A)
+print(A)
+print(len(A.shape))
+matrixA = np.random.rand(1, 10000)
+print(matrixA.shape)
 # for row in A:
 #     print(row)
 #     row = row.reshape(1, len(row))
