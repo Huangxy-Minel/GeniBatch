479fd26331d77327ae0ed15bd0acf046be03165b
Huangxy-Minel <huangxy_minel@foxmail.com>
2022-06-19 17:45:36 +0800

    update optim: HeteroGradient

--- fate_plugin/optim_hetero_gradients.py
+++ fate_plugin/optim_hetero_gradients.py
@@ -1,21 +1,22 @@
 import numpy as np
 from federatedml.util import LOGGER
 
-def __apply_cal_gradient_hp(data, fixed_point_encoder):
-    fore_gradient = data[1]
-    feature = np.array(data[0])
-    if fixed_point_encoder:
-        feature = fixed_point_encoder.encode(feature)
-    LOGGER.debug(f"feature shape: {feature.shape}")
-    LOGGER.debug(f"fore_gradient shape: {fore_gradient.get_shape()}")
-    # start_time = time.time()
-    gradient = fore_gradient.r_dot(np.ascontiguousarray(feature.transpose()))
-    # end_time = time.time()
-    # LOGGER.debug(f"dot time is: {end_time - start_time}")
-    all_g = fixed_point_encoder.decode(gradient)
-    return all_g
-
 class HeteroGradient(object):
+
+    @staticmethod
+    def __apply_cal_gradient_hp(data, fixed_point_encoder):
+        fore_gradient = data[1]
+        feature = np.array(data[0])
+        if fixed_point_encoder:
+            feature = fixed_point_encoder.encode(feature)
+        LOGGER.debug(f"feature shape: {feature.shape}")
+        LOGGER.debug(f"fore_gradient shape: {fore_gradient.get_shape()}")
+        # start_time = time.time()
+        gradient = fore_gradient.r_dot(np.ascontiguousarray(feature.transpose()))
+        # end_time = time.time()
+        # LOGGER.debug(f"dot time is: {end_time - start_time}")
+        all_g = fixed_point_encoder.decode(gradient)
+        return all_g
 
     @staticmethod
     def local_compute_gradient(data_instances, fore_gradient, fit_intercept, fixed_point_encoder):
@@ -27,7 +28,7 @@ class HeteroGradient(object):
             if data_count * feature_num > 100:
                 LOGGER.debug("Use apply partitions")
                 feat_join_grad = (data_instances, fore_gradient)
-                gradient_sum = __apply_cal_gradient_hp(feat_join_grad, fixed_point_encoder)
+                gradient_sum = HeteroGradient.__apply_cal_gradient_hp(feat_join_grad, fixed_point_encoder)
                 
                 if fit_intercept:
                     # bias_grad = np.sum(fore_gradient)
